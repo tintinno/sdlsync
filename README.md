7c0cf586eb5e04e52c4b86e1d31cf3a122ec41f9
tintinno <noemail@example.com>
2018-04-11 13:50:06 -0400

    adds directory-only sync

--- README.md
+++ README.md
@@ -6,8 +6,10 @@
 
 1. Install [ISHRemote](https://github.com/sdl/ISHRemote).
 1. Clone this repository.
-1. Open `sdlsync.ps1` and set the value for `$LocalRepository`.
-   This is the directory where you want to store a copy of items in the SDL repository.
+1. Open `sdlsync.ps1` and set the following values:
+  1. `$LocalRepository`: the local directory to which files are synced.
+  1. `$RemoteFolderPath`: the top-level directory you want to sync. Leave blank to sync everything.
+  1. (Optional) `$IshTypeFilter`: the IshTypes you want to sync.
 1. Open the file `netrc.ps1` and set the variables `$username`, `$password`, and `$url`. For example:
 ```
 $username = 'bob'
@@ -17,9 +19,9 @@ $url      = 'https://ccms.example.com/InfoShareWS/'
 
 ## Syncing SDL
 
-The first time you run `.\sdlsync.ps1`, the entire repository will sync to the specified
-directory. This will take several hours. The next time you run `sldsync.ps1`, only the
-files that were updated since the last run will be downloaded. 
+The first time you run `.\sdlsync.ps1`, the specified files are synced to the location set
+in `$LocalRepository`. (Syncing an entire repository takes several hours.) The next
+time you run `sldsync.ps1`, only the files that were updated since the last run are downloaded.
 
 After the initial sync, schedule the program to run as often as you want (e.g., every 
 10 minutes, every hour, or every night).
@@ -33,8 +35,6 @@ failed to download.
 
 ## Misc
 - Use the `-Verbose` flag to create verbose logs.
-- If you want to sync the whole repository again, delete the file `last-time-run.txt` and re-run the program.
+- If you want to sync the whole repository again, or if you've changed any of the intial
+variables, delete the file `last-time-run.txt` and re-run the program.
 
-## TODO
-- Let users specify a specific directory (rather than the whole repository) to sync.
-- Add fail warnings for topics that failed to download.
--- sdlsync.ps1
+++ sdlsync.ps1
@@ -7,37 +7,37 @@ param([Switch]$Verbose)
 ################################################################################################
 # Set the location of your local repository.
 # Filenames may exceed the Windows character limit, so keep this short (e.g., D:\repo). No final slash.
-$LocalRepository = 'D:\repo'
+$LocalRepository = ''
 
 # Set the IshTypeFilter if you only want certain types of files (e.g., maps and topics).
 # Example: $IshTypeFilter   = @('ISHMasterDoc','ISHModule')
 # Default: $IshTypeFilter   = @('ISHIllustration','ISHLibrary','ISHMasterDoc','ISHModule')
 $IshTypeFilter   = @('ISHIllustration','ISHLibrary','ISHMasterDoc','ISHModule')
 
-# TODO: Implement some reasonably efficient way to allow users to sync only specific directories.
-#
-# Set the location of the top-most directory you want to sync. Leave blank to sync everything. 
+# Set the location of the top-most directory you want to sync. Leave blank to sync everything.
 # Example: $RemoteFolderPath = '\General\New Releases\topics'
 # Default: $RemoteFolderPath = ''
-#$RemoteFolderPath = ''
+$RemoteFolderPath = ''
 
 ################################################################################################
 
 
-# if we encounter an error, write to log and screen, then exit; otherwise, only write to log
 function write-log ($msgtype, $msg){
+  # regardless of msgtype, write to log
+  Write-Output "[ $(Get-Date -UFormat "%Y-%m-%d %H:%M:%S") ] [$msgtype] $msg" | Out-File -Append -Encoding ascii sdl.log
+ 
+  # if msgtype is Error, echo to screen and exit; if Warn, echo to screen but do not exit
   if ( $msgtype -eq "ERROR"){
-    Write-Output "[ $(Get-Date -UFormat "%Y-%m-%d %H:%M:%S") ] [$msgtype] $msg" | Out-File -Append -Encoding ascii sdl.log
     Write-Output "[$msgtype] $msg"
     exit
-  } else {
-    Write-Output "[ $(Get-Date -UFormat "%Y-%m-%d %H:%M:%S") ] [$msgtype] $msg" | Out-File -Append -Encoding ascii sdl.log
+  } elseif ( $msgtype -eq "WARN"){
+    Write-Output "[$msgtype] $msg"
   }
 }
 
 # returns an ishSession
 function authenticate(){
-  
+ 
   if (Test-Path .\netrc.ps1){
     write-log "INFO" "Loading values from netrc.ps1"
     . .\netrc.ps1
@@ -53,8 +53,7 @@ function authenticate(){
 
 function validation(){
   if ( -Not $LocalRepository ){
-    echo "Open sdlsync and set the value of LocalRepository."
-    exit
+    write-log "ERROR" "Open sdlsync and set the value of LocalRepository."
   }
 }
 
@@ -62,7 +61,7 @@ function Set-RequestedMetadata(){
   Set-IshRequestedMetadataField -IshSession $session -Name FTITLE -Level Logical -ValueType Value |
   Set-IshRequestedMetadataField -IshSession $session -Name VERSION -Level Version -ValueType Value |
   Set-IshRequestedMetadataField -IshSession $session -Name DOC-LANGUAGE -Level Lng -ValueType Value |
-  Set-IshRequestedMetadataField -IshSession $session -Name MODIFIED-ON -Level Lng -ValueType Value 
+  Set-IshRequestedMetadataField -IshSession $session -Name MODIFIED-ON -Level Lng -ValueType Value
 }
 
 function Download-IshFile($ishdoc, $downloadPath){
@@ -90,9 +89,9 @@ function Download-IshDocs($ishdocs){
 function Download-AllFiles(){
   [datetime]::Now.ToString('d/M/yyyy HH:mm:ss') | Out-File -Encoding ascii .\last-time-run.txt
   $requestedMetadata = Set-RequestedMetadata
-  $ishdocs = Find-IshDocumentObj -IshSession $session `
-                -RequestedMetadata $requestedMetadata `
-                -IshTypeFilter $IshTypeFilter
+ 
+  # pass $null to Get-IshDocs because we have no date metadataFilter
+  $ishdocs = Get-IshDocs $null
   write-log "INFO" "Found $($ishdocs.count) new objects"
   Download-IshDocs $ishdocs
 }
@@ -110,18 +109,116 @@ function Download-ChangedFiles(){
   [datetime]::Now.ToString('d/M/yyyy HH:mm:ss') | Out-File -Encoding ascii .\last-time-run.txt
 
   # get all the topics modified after the date specified in last-time-run
-  $ishdocs = Find-IshDocumentObj -IshSession $session `
-                -RequestedMetadata $requestedMetadata `
-                -MetadataFilter $metadataFilter       `
-                -IshTypeFilter $IshTypeFilter
+  $ishdocs = Get-IshDocs $metadataFilter
   write-log "INFO" "Found $($ishdocs.count) updated objects"
   Download-IshDocs $ishdocs
+}
+
+function Get-TopicsInRemoteFolderPath(){
+  $ishdocs = @()
+  $folders = Get-IshFolder -IshSession $session -FolderPath $RemoteFolderPath -Recurse
+  foreach ($folder in $folders){
+    $content = Get-IshFolderContent -IshSession $session -FolderId $folder.IshFolderRef
+    if ($content){
+      $ishdocs += $content
+    }
+  }
+  # return
+  $ishdocs
+}
+
+function Filter-IshDocsByIshTypeFilter($temp){
+  $ishdocs = @()
+  foreach ($ishdoc in $temp){
+    if ($IshTypeFilter -contains $ishdoc.IshType){
+      $ishdocs += $ishdoc
+    }
+  }
+  # return
+  $ishdocs
+}
+
+function Filter-IshDocsByMetadataFilter($temp){
+  $ishdocs = @()
+  foreach ($ishdoc in $temp){
+    # if tempdoc has a value, then the topic matches the metadataFilter
+    $tempdoc = Get-IshDocumentObj -IshSession $session `
+                    -LogicalId $ishdoc.IshRef          `
+                    -MetadataFilter $metadataFilter
+    if ($tempdoc){
+      $ishdocs += $ishdoc
+    }
+  }
+  # return
+  $ishdocs
+}
+
+# make sure each item in ishdocs contains all necessary metadata
+function Add-IshDocMetadata($temp){
+  $ishdocs = @()
+  foreach ($tempfile in $temp){
+    $ishdoc = Get-IshDocumentObj -IshSession $session `
+                        -LogicalId $tempfile.IshRef   `
+                        -RequestedMetadata $requestedMetadata
+    $ishdocs += $ishdoc
+  }
+  # return
+  $ishdocs
+}
+
+# if $RemoteFolderPath has been set, use get-ishfoldercontent to create $ishdocs; otherwise,
+# use find-ishdocumentobj to create $ishdocs (where $ishdocs is the array of topics to sync)
+# The ishdocs returned depends on the state of 2 variables, so there are 4 combinations to
+# handle: 00, 01, 10, 11. Note that the IshTypeFilter is always present.
+function Get-IshDocs($metadataFilter){
+
+  # if RemoteFolderPath and metadataFilter have not been set, we want to sync the entire
+  # repository, so ishdocs should return everything
+  if (-not $RemoteFolderPath -and -not $metadataFilter){
+
+    $ishdocs = Find-IshDocumentObj -IshSession $session `
+                -RequestedMetadata $requestedMetadata   `
+                -IshTypeFilter $IshTypeFilter
+
+  # if RemoteFolderPath is not set but metadataFilter is, we should return everything in the
+  # repository that matches the metadata filter (i.e., that has changed since time-last-run)
+  } elseif (-not $RemoteFolderPath -and $metadataFilter){
+
+    $ishdocs = Find-IshDocumentObj -IshSession $session `
+                -RequestedMetadata $requestedMetadata   `
+                -MetadataFilter $metadataFilter         `
+                -IshTypeFilter $IshTypeFilter
+ 
+  # if RemoteFolderPath has been set but metadataFilter has not, ishdocs should contain all the
+  # items within RemoteFolderPath, recursively
+  } elseif ( $RemoteFolderPath -and -not $metadataFilter){
+
+    $ishdocs = Get-TopicsInRemoteFolderPath
+    $ishdocs = Filter-IshDocsByIshTypeFilter $ishdocs
+    $ishdocs = Add-IshDocMetadata $ishdocs
+ 
+  # if RemoteFolderPath and metadataFilter have been set, ishdocs should contain all the items
+  # within RemoteFolderPath, recursively, that match the metadata filter (i.e., that have
+  # changed since time-last-run)
+  } elseif ($RemoteFolderPath -and $metadataFilter){
+
+    $ishdocs = Get-TopicsInRemoteFolderPath
+    $ishdocs = Filter-IshDocsByMetadataFilter $ishdocs
+    $ishdocs = Filter-IshDocsByIshTypeFilter $ishdocs
+    $ishdocs = Add-IshDocMetadata $ishdocs
+      
+  } else {
+    write-log "ERROR" "Impossible combination of variables"
+  }
+
+  # return value
+  $ishdocs
 }
 
 function main(){
   $session = authenticate
   validation
-  
+ 
   if ( Test-Path .\last-time-run.txt ){
     Download-ChangedFiles
   } else {
@@ -130,3 +227,4 @@ function main(){
 }
 
 main
+
